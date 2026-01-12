d4c1b248887ac619903a728dca909e0fc4ad703e
cindolab <ringinbambu@gmail.com>
2026-01-12 12:22:12 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleAnggota() {
+    var more = document.getElementById('more-anggota');
+    if (more.style.display === 'none') {
+        more.style.display = 'block';
+    } else {
+        more.style.display = 'none';
+    }
+}
